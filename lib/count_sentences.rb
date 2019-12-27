811be68fc532df781afad1fa2116cae0b845f730
Oksana Nichols <oksanahnichols@yahoo.com>
2019-12-27 06:15:44 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,15 +3,15 @@ require 'pry'
 class String
 
   def sentence?
-    "HI." .end_with?(".")
+    "HI." .end_with.(".")
   end
 
   def question?
-     "HI" .end_with?("?")
+     "HI?" .end_with?("?")
   end
 
   def exclamation?
-
+     "Hi!" .end_with!("!")
   end
 
   def count_sentences
